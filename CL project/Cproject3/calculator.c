91e2e9cecb8259a227a5f7b8ae7216d75f984ed8
YnezArk <ynez_ark@163.com>
2025-10-30 20:07:00 +0800

    作业计算器

---
+++ CL project/Cproject3/calculator.c
@@ -0,0 +1,28 @@
+/*
+    Author: Ynez Ark
+    Date: 2025/10/30 19:50
+*/
+#include "stdio.h"
+int main(){
+    int num1, num2;
+    char op;
+    printf("输入算式\n");
+    scanf("%d %c %d",&num1,&op,&num2);
+    switch (op) {
+        case '+':
+            printf("%d %c %d = %d",num1,op,num2,num1+num2);
+            break;
+        case '-':printf("%d %c %d = %d",num1,op,num2,num1-num2);
+            break;
+        case '*':printf("%d %c %d = %d",num1,op,num2,num1*num2);
+            break;
+        case '/':
+            if(num2 != 0) printf("%d %c %d = %f",num1,op,num2,(num1*1.0/num2*1.0));
+            else printf("除数不能为零!");
+            break;
+        default:
+            printf("不存在的运算符");
+
+    }
+
+}
