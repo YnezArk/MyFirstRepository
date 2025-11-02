6ca920b48c9c1c10e0f9669a1f5bd264442cadc5
YnezArk <ynez_ark@163.com>
2025-11-02 22:14:55 +0800

    更新的IDE，更好的c项目，更好的calculator

--- CL project/Cproject3/calculator.c
+++ CL project/Cproject3/calculator.c
@@ -2,25 +2,27 @@
     Author: Ynez Ark
     Date: 2025/10/30 19:50
 */
+#include <stdlib.h>
+
 #include "stdio.h"
-#include "stdlib.h "
+
 
 int main(){
     system("chcp 65001 > nul");
-    int num1, num2;
+    double num1, num2;
     char op;
     printf("输入算式\n");
-    scanf("%d %c %d",&num1,&op,&num2);
+    scanf("%lf %c %lf",&num1,&op,&num2);
     switch (op) {
         case '+':
-            printf("%d %c %d = %d",num1,op,num2,num1+num2);
+            printf("%lf %c %lf = %lf",num1,op,num2,num1+num2);
             break;
-        case '-':printf("%d %c %d = %d",num1,op,num2,num1-num2);
+        case '-':printf("%lf %c %lf = %lf",num1,op,num2,num1-num2);
             break;
-        case '*':printf("%d %c %d = %d",num1,op,num2,num1*num2);
+        case '*':printf("%lf %c %lf = %lf",num1,op,num2,num1*num2);
             break;
         case '/':
-            if(num2 != 0) printf("%d %c %d = %f",num1,op,num2,(num1*1.0/num2*1.0));
+            if(num2 != 0) printf("%lf %c %lf = %lf",num1,op,num2,(num1*1.0/num2));
             else printf("除数不能为零!");
             break;
         default:
